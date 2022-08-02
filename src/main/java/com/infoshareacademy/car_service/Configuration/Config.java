422dd572d5aa3dc452fe0f32fafeef367dc4a322
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-08-03 00:24:39 +0200

    Add config class

---
+++ src/main/java/com/infoshareacademy/car_service/Configuration/Config.java
@@ -0,0 +1,30 @@
+package com.infoshareacademy.car_service.Configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        return resourceBundleMessageSource;
+
+    }
+
+    @Bean
+   public LocaleResolver localeResolver() {
+       SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+      sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+       return sessionLocaleResolver;
+   }
+}
