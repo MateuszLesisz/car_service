f76f9e35c49326318bdee136f0d8cd14000630fe
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-08-24 15:09:15 +0200

    New objects from form dont override json objects - all works correctly

--- src/main/java/com/infoshareacademy/car_service/repository/CarRepository.java
+++ src/main/java/com/infoshareacademy/car_service/repository/CarRepository.java
@@ -4,9 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.infoshareacademy.car_service.Model.Car;
-import com.infoshareacademy.car_service.service.CarService;
 import com.infoshareacademy.car_service.utils.GsonLocalDate;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
@@ -15,9 +13,7 @@ import java.io.FileReader;
 import java.io.Reader;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
@@ -25,8 +21,8 @@ public class CarRepository extends FakeRepository<Long, Car> {
 
     private static final String CAR_REPOSITORY_JSON_FILE = "src/main/resources/carsForRepair.json";
     private final Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new GsonLocalDate()).setPrettyPrinting().create();
+    private final AtomicLong nextId = new AtomicLong();
 
-    private final AtomicLong nextId = new AtomicLong(1L);
     @PostConstruct
     public void initDb() {
         try {
@@ -38,7 +34,7 @@ public class CarRepository extends FakeRepository<Long, Car> {
 
     @Override
     Long nextId() {
-        return nextId.incrementAndGet();
+        return nextId.addAndGet(fakeDb.size() + 1L);
     }
 
     public void loadFromFile() {
@@ -46,7 +42,7 @@ public class CarRepository extends FakeRepository<Long, Car> {
             Reader reader = new FileReader(CAR_REPOSITORY_JSON_FILE);
             List<Car> cars = gson.fromJson(reader, new TypeToken<ArrayList<Car>>() {
             }.getType());
-            cars.forEach(car -> fakeDb.put(car.getId(),car));
+            cars.forEach(car -> fakeDb.put(car.getId(), car));
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
