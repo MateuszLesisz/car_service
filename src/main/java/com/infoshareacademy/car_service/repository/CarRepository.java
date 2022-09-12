b9cf6b41c30ce7df69772016abe9b75cff13f657
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-09-12 21:52:42 +0200

    Add repository annotation

--- src/main/java/com/infoshareacademy/car_service/repository/CarRepository.java
+++ src/main/java/com/infoshareacademy/car_service/repository/CarRepository.java
@@ -2,10 +2,11 @@ package com.infoshareacademy.car_service.repository;
 
 import com.infoshareacademy.car_service.Model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-import java.util.Optional;
 
+@RepositoryRestResource
 public interface CarRepository extends JpaRepository<Car, Long> {
 
     List<Car> findCarsByIsFixed(Boolean isFixed);
