ae8d4e78ddb33c8c3fd2d13b890d33eed38ffc0e
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-10-13 18:24:03 +0200

    Add findByRegistrationNumber

--- src/main/java/com/infoshareacademy/car_service/repository/CarRepository.java
+++ src/main/java/com/infoshareacademy/car_service/repository/CarRepository.java
@@ -11,4 +11,6 @@ public interface CarRepository extends JpaRepository<Car, Long> {
 
     List<Car> findCarsByIsFixed(Boolean isFixed);
 
+    Car findByRegistrationNumber(String registrationNumber);
+
 }
