6015058e285cd0a4a7ae884f417f618db8afa117
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-10-18 10:14:04 +0200

    Save of cars saved each day

--- src/main/java/com/infoshareacademy/car_service/service/CarService.java
+++ src/main/java/com/infoshareacademy/car_service/service/CarService.java
@@ -23,7 +23,7 @@ public class CarService {
     private final CarRepository carRepository;
     private static final String BROKEN_CAR_REPOSITORY_JSON_FILE = "src/main/resources/brokenCar.json";
 
-    private static final String FIXED_CAR_REPOSITORY_JSON_FILE = "src/main/resources/fixed_cars/fixedCars.json";
+    private static final String FIXED_CAR_REPOSITORY_JSON_FILE = "src/main/resources/fixed_cars/" + LocalDate.now() + " " + "fixedCars.json";
     private final Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new GsonLocalDate()).setPrettyPrinting().create();
 
     public void saveToFileBrokenCars() {
