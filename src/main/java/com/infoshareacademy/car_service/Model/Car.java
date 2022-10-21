74ffccc316a665686cebd3b2679f5ef513a5f41a
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-10-21 21:39:50 +0200

    Revert "Add fixed_car table"
    This reverts commit cc5a10835fd44c8e62f68b9d1a9110da4e674f64.

--- src/main/java/com/infoshareacademy/car_service/Model/Car.java
+++ src/main/java/com/infoshareacademy/car_service/Model/Car.java
@@ -1,7 +1,10 @@
 package com.infoshareacademy.car_service.Model;
 
 import com.infoshareacademy.car_service.dto.Color;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -15,40 +18,32 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Car {
 
-    public static final String TABLE_NAME = "broken_car";
+    public static final String TABLE_NAME = "car";
 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter
     private Long id;
 
     @Column(name = "registration_number", nullable = false)
-    @Setter
     private String registrationNumber;
 
     @Column(name = "name", nullable = false)
-    @Setter
     private String name;
 
     @Column(name = "color", nullable = false)
-    @Setter
     private Color color;
 
     @Column(name = "year_of_production", nullable = false)
-    @Setter
     private Integer yearOfProduction;
 
     @Column(name = "dateOfAddCar", nullable = false)
-    @Setter
     private LocalDate dateOfAddCar;
 
     @Column(name = "fixed", nullable = false)
-    @Setter
     private Boolean isFixed;
 
     @Column(name = "fixedDate")
-    @Setter
     private LocalDate fixedDate;
 
     public Car(String registrationNumber, String name, Color color, Integer yearOfProduction, LocalDate dateOfAddCar, Boolean isFixed, LocalDate fixedDate) {
@@ -59,5 +54,6 @@ public class Car {
         this.dateOfAddCar = dateOfAddCar;
         this.isFixed = isFixed;
         this.fixedDate = fixedDate;
+
     }
 }
--- src/main/resources/db/migration/V1__init_broken_car_table.sql
+++ src/main/resources/db/migration/V1__init_broken_car_table.sql
@@ -1,5 +1,5 @@
 
-create table broken_car
+create table car
 (
     id                 bigint primary key auto_increment,
     registrationNumber varchar(100) not null,
