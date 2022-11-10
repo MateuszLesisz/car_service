3e4e1ad93f01164b5da25be72ca2448286903bb2
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-11-10 11:43:16 +0100

    Add some validations

--- src/main/java/com/infoshareacademy/car_service/dto/BrokenCarDto.java
+++ src/main/java/com/infoshareacademy/car_service/dto/BrokenCarDto.java
@@ -2,8 +2,7 @@ package com.infoshareacademy.car_service.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 
@@ -12,11 +11,15 @@ public class BrokenCarDto {
 
     private Long id;
     @NotBlank(message = "Registration number field cannot be empty")
+    @Size(min = 7, message = "registration number is too short")
     private String registrationNumber;
     @NotBlank(message = "name field cannot be empty")
+    @Size(min = 2, message = "name is too short")
     private String name;
     private Color color;
     @NotNull(message = "year of production field cannot be empty")
+    @Max(2022)
+    @Min(1800)
     private Integer yearOfProduction;
     private LocalDate dateOfAddCar;
     private Boolean isFixed;
