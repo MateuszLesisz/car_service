fda456fe1d84abb928550dc8f16e12de38095acf
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-11-27 20:22:59 +0100

    Add UserDto class

---
+++ src/main/java/com/infoshareacademy/car_service/dto/User.java
@@ -0,0 +1,27 @@
+package com.infoshareacademy.car_service.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class User {
+
+    private Long id;
+    @NotBlank(message = "name field cannot be empty")
+    private String name;
+    @NotBlank(message = "lastname field cannot be empty")
+    private String lastname;
+    @NotBlank(message = "e-mail field cannot be empty")
+    private String email;
+    @NotBlank(message = "password field cannot be empty")
+    private String password;
+    @NotBlank(message = "city field cannot be empty")
+    private String city;
+    @NotBlank(message = "street field cannot be empty")
+    private String street;
+    @NotBlank(message = "number/local field cannot be empty")
+    private String numberAndlocal;
+    @NotBlank(message = "zip code field cannot be empty")
+    private int zipCode;
+}
