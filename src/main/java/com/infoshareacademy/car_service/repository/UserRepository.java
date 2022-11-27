22d7f3d8c17b3c5f52d85a3ef11242a84a711a2f
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-11-27 20:31:09 +0100

    create UserRepository interface

---
+++ src/main/java/com/infoshareacademy/car_service/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy.car_service.repository;
+
+import com.infoshareacademy.car_service.dto.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
