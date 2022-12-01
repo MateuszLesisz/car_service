d94c8b704f67f0396dce4ff8e538f531ffab9c7e
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-12-01 22:42:58 +0100

    Revert "Add EmailConfirmation"
    This reverts commit 78c710355ae0a49a55bc4b43a984155520f7c72c.

--- src/main/java/com/infoshareacademy/car_service/repository/EmailConfirmationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infoshareacademy.car_service.repository;
-
-import com.infoshareacademy.car_service.model.EmailConfirmation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailConfirmationRepository extends JpaRepository<EmailConfirmation, Long> {
-}
