30c9f3b149c1713fbd7468a2d6360ff66f05718b
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-12-06 16:02:18 +0100

    Add login by credentials

--- src/main/java/com/infoshareacademy/car_service/security/SecurityConfiguration.java
+++ src/main/java/com/infoshareacademy/car_service/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -32,5 +33,15 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        super.configure(httpSecurity);
+        httpSecurity.authorizeHttpRequests()
+                .antMatchers("/welcomePage")
+                .hasRole("USER")
+                .anyRequest()
+                .permitAll();
     }
 }
