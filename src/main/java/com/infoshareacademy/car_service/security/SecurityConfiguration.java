3edaebb3d1e44ac21b07e8aae03e49c773384bb5
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-12-06 15:18:45 +0100

    Add global config to SecurityConfiguration class

--- src/main/java/com/infoshareacademy/car_service/security/SecurityConfiguration.java
+++ src/main/java/com/infoshareacademy/car_service/security/SecurityConfiguration.java
@@ -2,8 +2,12 @@ package com.infoshareacademy.car_service.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -13,6 +17,16 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
     @Bean
     public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Autowired
+    void configureGlobal(AuthenticationManagerBuilder auth) {
+        var authorityMapper = new SimpleAuthorityMapper();
+        authorityMapper.setPrefix("ROLE_");
+        authorityMapper.setConvertToUpperCase(true);
+        KeycloakAuthenticationProvider keycloakProvider = keycloakAuthenticationProvider();
+        keycloakProvider.setGrantedAuthoritiesMapper(authorityMapper);
+        auth.authenticationProvider(keycloakProvider);
     }
 
     @Override
