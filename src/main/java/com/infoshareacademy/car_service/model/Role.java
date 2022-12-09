d182ec7e355b8e92a0fc94a1e41170d2fd957b44
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-12-09 22:02:01 +0100

    Create Role class, update user table

---
+++ src/main/java/com/infoshareacademy/car_service/model/Role.java
@@ -0,0 +1,25 @@
+package com.infoshareacademy.car_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column
+    private String name;
+}
