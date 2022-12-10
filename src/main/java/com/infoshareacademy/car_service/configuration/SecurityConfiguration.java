e746f6310d8cdba6a3ce9c48371ea9164ef8c25c
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-12-10 14:58:10 +0100

    Revert "Refactor"
    This reverts commit 2a363b2fd61e7b605e7f917cbe046425f76980ee.

--- src/main/java/com/infoshareacademy/car_service/configuration/SecurityConfiguration.java
+++ src/main/java/com/infoshareacademy/car_service/configuration/SecurityConfiguration.java
@@ -20,7 +20,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private UserService userService;
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
